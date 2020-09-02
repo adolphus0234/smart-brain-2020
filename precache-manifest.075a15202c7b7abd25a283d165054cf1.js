2799faf0cc94f1fc986a2893388ae7d58a751e9c
Your Name <you@example.com>
2020-09-01 18:07:50 -0600

    Updates

---
+++ precache-manifest.075a15202c7b7abd25a283d165054cf1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e6cea99bcc72a01b177c27bf808feec",
+    "url": "/smart-brain-2020/index.html"
+  },
+  {
+    "revision": "b98d33565f95f58fe9e4",
+    "url": "/smart-brain-2020/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "4097ac0656f763baafee",
+    "url": "/smart-brain-2020/static/css/main.70309b87.chunk.css"
+  },
+  {
+    "revision": "b98d33565f95f58fe9e4",
+    "url": "/smart-brain-2020/static/js/2.a643c3a7.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/smart-brain-2020/static/js/2.a643c3a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4097ac0656f763baafee",
+    "url": "/smart-brain-2020/static/js/main.e3331a84.chunk.js"
+  },
+  {
+    "revision": "74a4aa3f526d4b804824",
+    "url": "/smart-brain-2020/static/js/runtime-main.46206cde.js"
+  }
+]);
